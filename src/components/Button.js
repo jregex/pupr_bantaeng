527ffcb94d6430d796d9410912870397838d27db
jregex <cakghofur3@gmail.com>
2023-05-14 14:01:04 +0800

    bayangan tombol

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 const Button = ({ type = 'submit', className, ...props }) => (
     <button
         type={type}
-        className={`${className} inline-flex items-center px-4 py-2 bg-primary border border-transparent rounded-full font-semibold text-sm text-slate-800 tracking-widest hover:opacity-80 active:bg-sky-900 focus:outline-none focus:border-primary focus:ring ring-sky-300 disabled:opacity-25 transition shadow-lg shadow-sky-400 hover:shadow-sm active:shadow focus:shadow ease-in-out duration-300`}
+        className={`${className} inline-flex items-center px-4 py-2 bg-primary border border-transparent rounded-full font-semibold text-sm text-slate-800 tracking-widest hover:opacity-80 active:bg-sky-900 focus:outline-none focus:border-primary focus:ring ring-sky-300 disabled:opacity-25 transition shadow-md shadow-sky-300 hover:shadow-sm active:shadow focus:shadow ease-in-out duration-300`}
         {...props}
     />
 )
--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -26,7 +26,7 @@ export default function NewsCard({ alamat, gambar, judul, desc, alt }) {
                 </p>
                 <div className="flex justify-end">
                     <Link href={alamat} legacyBehavior>
-                        <a className="inline-flex items-center px-4 py-2 bg-primary border border-transparent rounded-full font-bold text-sm text-dark tracking-widest hover:opacity-80 active:bg-sky-900 focus:outline-none focus:border-primary focus:ring ring-sky-300 disabled:opacity-25 transition shadow-lg shadow-sky-400 hover:shadow-sm active:shadow focus:shadow ease-in-out duration-300">
+                        <a className="inline-flex items-center px-4 py-2 bg-primary border border-transparent rounded-full font-bold text-sm text-dark tracking-widest hover:opacity-80 active:bg-sky-900 focus:outline-none focus:border-primary focus:ring ring-sky-300 disabled:opacity-25 transition shadow-md shadow-sky-300 hover:shadow-sm active:shadow focus:shadow ease-in-out duration-300">
                             Baca selengkapnya
                         </a>
                     </Link>
