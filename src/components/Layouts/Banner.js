7ca39ac2aa8b70e0ca4682036f280aed5f1b9459
jregex <cakghofur3@gmail.com>
2023-06-28 16:58:12 +0800

    warna banner

--- src/components/Layouts/Banner.js
+++ src/components/Layouts/Banner.js
@@ -16,8 +16,8 @@ export default function Banner({ nama }) {
                                 {nama}
                             </h5>
                         </div>
-                        <div className="absolute top-20 right-20 lg:right-[500px] rounded-full bg-primary w-[80px] h-[80px] lg:w-[120px] lg:h-[120px]  -z-1 filter blur-3xl opacity-70 animate-pulse" />
-                        <div className="absolute bottom-10 right-20 lg:right-[500px] rounded-full bg-pink-500 w-[80px] h-[80px] lg:w-[120px] lg:h-[120px]  -z-1 filter blur-3xl opacity-70 animate-pulse" />
+                        <div className="absolute top-20 right-20 lg:right-[500px] rounded-full bg-primary w-[80px] h-[80px] lg:w-[120px] lg:h-[120px]  -z-1 filter blur-3xl opacity-90 animate-pulse" />
+                        <div className="absolute bottom-10 right-20 lg:right-[500px] rounded-full bg-pink-500 w-[80px] h-[80px] lg:w-[120px] lg:h-[120px]  -z-1 filter blur-3xl opacity-90 animate-pulse" />
                     </div>
                 </div>
             </div>
